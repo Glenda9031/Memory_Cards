a7c2bb6765cbf7834ca5a543f78d8fe131dfc8bd
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2026-02-24 16:42:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ const cardsEl = [];
 // Store card data
 const cardsData = getCardsData();
 
+const cardsData = [
+    {
+        question: 'What must a variable begin with?',
+        answer: 'A letter, $ or _'
+    }
+    {
+        question: 'What is a variable?',
+        answer: 'Container for a piece of data'
+    }
+    {
+        question: 'Example of Case Sensitive Variable',
+        answer: 'thisIsAVariable'
+    }
+];
+
